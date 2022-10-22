d9f12c78b7734e4d5e06cbe07e98155497d7876f
Bidisha Das <officialbidisha1@gmail.com>
2022-10-22 16:31:35 +0530

    Added carousal with images..

--- src/components/CarousalItem.js
+++ src/components/CarousalItem.js
@@ -1,11 +1,19 @@
 import "./CarousalItem.css";
 const CarousalItem = (props) => {
-    const {item, width} = props;
-    const {avatar,content,href,name,id} = item;
-    return (
-        <div className="carousal-item" style={{width: props.width}}>
-            <p>{content}</p>
-        </div>
-    )
-}
+  const { item, width } = props;
+  const { avatar, content, href, name} = item;
+  return (
+    <div
+      className="carousal-item"
+      style={{ width: width, backgroundImage: `url(${avatar})` }}
+    >
+      <div className="container">
+        <p className="text">{content}</p>
+
+        <a href={href}>Reference</a>
+        <p style={{color: "white"}}>{name}</p>
+      </div>
+    </div>
+  );
+};
 export default CarousalItem;
